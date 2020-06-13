8d757e264c979a5e5670fdcaaa11ef3c0b31ba81
Qunhua.Liao <lqh@mingcloud.com>
2020-06-13 17:31:04 +0800

    发布新版本：复制流程设置参数

--- quinn-service-server/src/main/java/com/quinn/framework/component/SpringLicenceApplicationDecorator.java
+++ quinn-service-server/src/main/java/com/quinn/framework/component/SpringLicenceApplicationDecorator.java
@@ -1,6 +1,7 @@
 package com.quinn.framework.component;
 
 import com.quinn.framework.api.SpringApplicationDecorator;
+import com.quinn.util.base.CollectionUtil;
 import com.quinn.util.base.constant.ConfigConstant;
 import com.quinn.util.base.enums.CommonMessageEnum;
 import com.quinn.util.base.factory.LicenceClassLoader;
@@ -12,6 +13,8 @@ import com.quinn.util.constant.enums.SystemExitTypeEnum;
 import com.quinn.util.licence.model.ApplicationInfo;
 import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import java.io.File;
 import java.net.URL;
@@ -36,15 +39,20 @@ public class SpringLicenceApplicationDecorator implements SpringApplicationDecor
         if (!file.exists()) {
             if (licenceDir.startsWith(ConfigConstant.CONFIG_KEY_PREFIX_CLASSPATH) ||
                     licenceDir.startsWith(ConfigConstant.CONFIG_KEY_PREFIX_CLASSPATH_ALL)) {
-                licenceDir = licenceDir.substring(licenceDir.indexOf(StringConstant.CHAR_COLON) + 1);
-                file = new File(licenceDir);
+
+                String licenceDirTemp = licenceDir.substring(licenceDir.indexOf(StringConstant.CHAR_COLON) + 1);
+                file = new File(licenceDirTemp);
                 if (file.exists()) {
                     resource = file.toURI().toURL();
                 }
             }
 
             if (resource == null) {
-                resource = SpringLicenceApplicationDecorator.class.getClassLoader().getResource(licenceDir);
+                PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+                Resource[] resources = resolver.getResources(licenceDir);
+                if (!CollectionUtil.isEmpty(resources)) {
+                    resource = resources[0].getURL();
+                }
             }
         } else {
             resource = file.toURI().toURL();
