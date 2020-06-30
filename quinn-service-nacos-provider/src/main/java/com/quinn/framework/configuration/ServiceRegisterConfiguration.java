1a594fca2b35930db0074f1300229bb815bd97ea
Qunhua.Liao <lqh@mingcloud.com>
2020-06-30 15:55:42 +0800

    注册中心

---
+++ quinn-service-nacos-provider/src/main/java/com/quinn/framework/configuration/ServiceRegisterConfiguration.java
@@ -0,0 +1,15 @@
+package com.quinn.framework.configuration;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 服务注册配置类
+ *
+ * @author Qunhua.Liao
+ * @since 2020-06-30
+ */
+@Configuration
+@EnableDiscoveryClient
+public class ServiceRegisterConfiguration {
+}
