75c26e9718377277da0355897ec2e74c1ec79b95
Quinn.Liao <bjqh2011@126.com>
2020-08-02 17:20:51 +0800

    构造优化

--- quinn-service-scheduler/src/main/java/com/quinn/framework/configuration/ScheduleConfiguration.java
+++ quinn-service-scheduler/src/main/java/com/quinn/framework/configuration/ScheduleConfiguration.java
@@ -3,7 +3,6 @@ package com.quinn.framework.configuration;
 import com.quinn.framework.api.BusinessJob;
 import com.quinn.framework.component.JobLoadOnStartedListener;
 import com.quinn.framework.component.TestBusinessJob;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,7 +22,6 @@ public class ScheduleConfiguration {
     }
 
     @Bean("jobLoadOnStartedListener")
-    @ConditionalOnBean(name = {"jobHelpService", "jobExecuteService"})
     public ApplicationListener jobLoadOnStartedListener() {
         return new JobLoadOnStartedListener();
     }
